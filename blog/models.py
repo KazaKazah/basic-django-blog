a5ed86eafe70df43bcca5525a0cc2ac4f007320c
Asanali Yeltayev <me@pythonworld.ru>
2022-09-28 19:56:24 +0600

    1

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,7 @@
 from datetime import datetime
-
+from tabnanny import verbose
 from django.db import models
-
 from autoslug import AutoSlugField
-
 from PIL import Image
 
 
@@ -13,7 +11,15 @@ def get_category_id():
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name="наименование"
+    )
+
+    class Meta:
+        verbose_name = 'тег'
+        verbose_name_plural = 'теги'
 
     def __str__(self):
         return self.name
@@ -21,10 +27,11 @@ class Tag(models.Model):
 
 class Category(models.Model):
 
-    DEFAULT_NAME = "Uncategorised"
+    DEFAULT_NAME = ""
 
     class Meta:
-        verbose_name_plural = "categories"
+        verbose_name = 'категория'
+        verbose_name_plural = "категории"
 
     name = models.CharField(max_length=100, unique=True)
     slug = AutoSlugField(populate_from="name", unique=True)
@@ -40,17 +47,23 @@ class Post(models.Model):
         ("published", "Published"),
     ]
 
-    title = models.CharField(max_length=255)
+    title = models.CharField(
+        max_length=255,
+        verbose_name='наименование'
+        )
     slug = AutoSlugField(populate_from="title", unique=True)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="draft")
+    status = models.CharField(
+        max_length=20, choices=STATUS_CHOICES, default="draft")
 
-    body = models.TextField()
+    body = models.TextField(
+        verbose_name='описнаие'
+    )
 
-    created_date = models.DateTimeField(auto_now_add=True)
-    published_date = models.DateTimeField(blank=True, null=True)
-    updated_date = models.DateTimeField(blank=True, null=True)
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    published_date = models.DateTimeField(blank=True, null=True, verbose_name='дата публикации')
+    updated_date = models.DateTimeField(blank=True, null=True, verbose_name='дата дата обновления')
 
-    tags = models.ManyToManyField(to=Tag, related_name="posts", blank=True)
+    tags = models.ManyToManyField(to=Tag, related_name="posts", blank=True, verbose_name='теги')
     category = models.ForeignKey(
         to=Category,
         related_name="posts",
@@ -58,9 +71,15 @@ class Post(models.Model):
         on_delete=models.SET_DEFAULT,
         blank=False,
         null=False,
+        verbose_name='категория'
     )
 
-    feature_image = models.ImageField(upload_to="uploads/", null=True, blank=True)
+    class Meta:
+        verbose_name = 'запись'
+        verbose_name_plural = 'записи'
+
+    feature_image = models.ImageField(
+        upload_to="uploads/", null=True, blank=True, verbose_name='картинка')
 
     @property
     def excerpt(self):
